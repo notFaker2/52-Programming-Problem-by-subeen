67c287763398ec3ead38d10edfb623c283687aa7
notFaker2 <148624131+notFaker2@users.noreply.github.com>
2024-01-14 15:55:49 +0600

    Add files via upload

---
+++ Problem-1/Odd_Even_1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int t; cin>>t;
+    if(t%2){
+        cout<<"odd"<<endl;
+    }
+    else{
+        cout<<"even"<<endl;
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    while(n--){
+        solve();
+    }
+}
