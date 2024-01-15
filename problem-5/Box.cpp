87b46e0d74ca8989ed1869ddb2749fb8d84fdea1
notFaker2 <md.abdullacse20@gmail.com>
2024-01-15 17:01:52 +0600

    Problem-5 added

---
+++ problem-5/Box.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
